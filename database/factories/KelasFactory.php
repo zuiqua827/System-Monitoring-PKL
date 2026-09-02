782fd8410150e4981f6c0565290dc42775c96900
Muhammad Syahril ROmadhon <ariljepara08@gmail.com>
2026-09-02 16:38:29 +0700

    perbarui bug

--- database/factories/KelasFactory.php
+++ database/factories/KelasFactory.php
@@ -25,7 +25,7 @@ class KelasFactory extends Factory
         $tingkat = fake()->randomElement([10, 11, 12]);
 
         return [
-            'jurusan_id' => Jurusan::factory(),
+            'jurusan_id' => Jurusan::inRandomOrder()->value('id'),
             'nama' => sprintf('%d %s', $tingkat, fake()->unique()->bothify('??-#')),
             'tingkat' => $tingkat,
             'tahun_ajaran' => fake()->randomElement(['2025/2026', '2026/2027']),
